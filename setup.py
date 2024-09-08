6d0242d227fbbaef71d38ff995ea81100f29bab1
karim-da <karim.dahmeni@deepmetis.com>
2024-09-08 01:27:08 +0100

    learning :')

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="hospital-client",
     version="0.1.3",
     packages=find_packages(),
-    install_requires=["aiohttp==3.10.5", "pydantic==2.9.0", "pycryptodome==3.20.0"],
+    install_requires=["aiohttp>=3.10.5", "pydantic>=2.5.0", "pycryptodome>=3.15.0"],
     author="Karim Dahmeni",
     author_email="karim.dahmeni@deepmetis.com",
     description="A client for interacting with the service hospital",
